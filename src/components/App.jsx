f21185072da4c7675ee2687e4fe9a9f2ea6f030a
Alan Cleetus <alanclee95@gmail.com>
2024-07-06 13:57:14 -0700

    added darkmode support

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
-import Footer from "./Footer";
 import Note from "./Note";
 import notesList from "/notes";
 import CreateArea from "./CreateArea";
 
 let App = () => {
-  // App component Start
+  const [theme, setTheme] = useState(() => {
+    return localStorage.getItem("theme") || "light";
+  });
 
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme); // Apply the theme to the document
+    localStorage.setItem("theme", theme); // Save theme to local storage
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light"); // Toggle between light and dark themes
+  };
   // State hook to manage the list of notes, initialized with notesList
   const [notes, setNotes] = React.useState([...notesList]);
 
@@ -31,11 +40,10 @@ let App = () => {
 
   return (
     <>
-      <Header />
+      <Header toggleTheme={toggleTheme} theme={theme} />
 
       {/* Render the CreateArea component and pass the addNote function as a prop */}
       <CreateArea addNote={addNote} />
-
       {/* Create a Note component for each note in the notesList*/}
       <div className="centered-notes-container">
         {notes.map((note) => (
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import HighlightIcon from "@mui/icons-material/Highlight";
-function Header() {
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness5Icon from "@mui/icons-material/Brightness5";
+function Header({ toggleTheme, theme }) {
   return (
     <header>
       <h1>
         <HighlightIcon></HighlightIcon>Keeper
       </h1>
+      <button className="dark-mode-button" onClick={toggleTheme}>
+        {theme === "light" ? <Brightness4Icon /> : <Brightness5Icon />}{" "}
+      </button>
     </header>
   );
 }
