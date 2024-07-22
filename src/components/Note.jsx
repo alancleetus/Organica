94e841aa8f2f0f394fb966d9c7f3097331322562
Alan Cleetus <alanclee95@gmail.com>
2024-07-22 14:36:16 -0700

    Adding badges to count number of unchecked items

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -8,6 +8,9 @@ import CheckboxList from "./DNDCheckboxList";
 import { v4 as uuidv4 } from "uuid";
 import AddIcon from "@mui/icons-material/Add";
 import Grid from "@mui/material/Grid";
+import Badge from "@mui/material/Badge";
+import { styled } from "@mui/material/styles";
+
 function note(props) {
   const [isEditing, setIsEditing] = useState(false);
   const [editedTitle, setEditedTitle] = useState(props.title);
@@ -30,16 +33,28 @@ function note(props) {
 
   const handleSaveClick = () => {
     if (props.isList) {
-      //const updatedItems = itemsArray.filter((item) => item.text != "");
+      //const updatedItems = itemsArray.filter((item) => item.text !== "");
       const updatedItems = itemsArray;
       setItemsArray(updatedItems);
       //console.log("removing empty item");
       props.editNote(props.id, editedTitle, updatedItems);
-    } else if (editedTitle !== props.title || editedContent !== props.content)
+    } else if (editedTitle !== props.title || editedContent !== props.content) {
       props.editNote(props.id, editedTitle, editedContent);
+    }
 
     setIsEditing(false);
   };
+
+  const StyledBadge = styled(Badge)(({ theme }) => ({
+    "& .MuiBadge-badge": {
+      right: -20,
+      top: 10,
+      padding: "0 4px",
+      color: "var(--primary-color)",
+      background: "var(--create-note-bg-color)",
+      fontSize: "13px",
+    },
+  }));
 
   return (
     <div className="note">
@@ -88,14 +103,27 @@ function note(props) {
         </>
       ) : (
         <>
-          <h1>{props.title}</h1>
           {props.isList ? (
-            <CheckboxList
-              itemsArray={itemsArray}
-              updateItemsArray={updateItemsArray}
-            />
+            <>
+              <StyledBadge
+                badgeContent={Array.from(itemsArray).reduce((acc, item) => {
+                  if (!item.checked) acc += 1;
+                  return acc;
+                }, 0)}
+              >
+                <h1>{props.title}</h1>
+              </StyledBadge>
+
+              <CheckboxList
+                itemsArray={itemsArray}
+                updateItemsArray={updateItemsArray}
+              />
+            </>
           ) : (
-            <p>{props.content}</p>
+            <>
+              <h1>{props.title}</h1>
+              <p>{props.content}</p>{" "}
+            </>
           )}
 
           <button className="editButton" onClick={handleEditClick}>
