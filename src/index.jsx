9303f4882781ab7ed94045244a03663403608ece
Alan Cleetus <alanclee95@gmail.com>
2024-12-13 11:56:36 -0800

    Modularizing styles and adding new styles

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,15 @@ import { createRoot } from "react-dom/client";
 import App from "./components/App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
+import "../public/styles/styles.css";
+import "../public/styles/badge.css";
+import "../public/styles/header.css";
+import "../public/styles/loginRegister.css";
+import "../public/styles/modal.css";
+import "../public/styles/note.css";
+import "../public/styles/noteContainer.css";
+import "../public/styles/sorter.css";
+import "../public/styles/section.css";
 // Get the root element
 const rootElement = document.getElementById("root");
 
