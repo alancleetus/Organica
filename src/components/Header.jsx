d75cb0c4e04181efd4013597ffe14cc6fdb2b775
Alan Cleetus <alanclee95@gmail.com>
2024-12-13 11:58:25 -0800

    Export notes button and minor changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import HighlightIcon from "@mui/icons-material/Highlight";
-import Brightness4Icon from "@mui/icons-material/Brightness4";
-import Brightness5Icon from "@mui/icons-material/Brightness5";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import LightModeIcon from "@mui/icons-material/LightMode";
 import { Logout } from "@mui/icons-material";
 import { handleLogout } from "./auth/Logout";
+import FileDownloadIcon from "@mui/icons-material/FileDownload";
 import WifiOffIcon from "@mui/icons-material/WifiOff";
 import WifiIcon from "@mui/icons-material/Wifi";
 function Header({ toggleTheme, theme }) {
@@ -22,6 +23,36 @@ function Header({ toggleTheme, theme }) {
       window.removeEventListener("offline", updateOnlineStatus);
     };
   }, []);
+
+  const exportNotes = async () => {
+    try {
+      // Fetch all notes
+      const notesCollection = collection(db, "notes");
+      const q = query(notesCollection, where("userId", "==", user.uid));
+      const notesSnapshot = await getDocs(q);
+      const notesList = notesSnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+
+      // Create a JSON blob
+      const notesBlob = new Blob([JSON.stringify(notesList, null, 2)], {
+        type: "application/json",
+      });
+
+      // Create a download link and trigger it
+      const link = document.createElement("a");
+      link.href = URL.createObjectURL(notesBlob);
+      link.download = "notes_backup.json";
+      link.click();
+      URL.revokeObjectURL(link.href);
+
+      console.log("Notes exported successfully!");
+    } catch (error) {
+      console.error("Error exporting notes:", error);
+    }
+  };
+
   return (
     <header>
       <h1>
@@ -35,9 +66,17 @@ function Header({ toggleTheme, theme }) {
       <button
         className="dark-mode-button"
         style={{ marginRight: "10px" }}
+        onClick={exportNotes}
+      >
+        <FileDownloadIcon />
+      </button>
+
+      <button
+        className="dark-mode-button"
+        style={{ marginRight: "10px" }}
         onClick={toggleTheme}
       >
-        {theme === "light" ? <Brightness4Icon /> : <Brightness5Icon />}{" "}
+        {theme === "light" ? <DarkModeIcon /> : <LightModeIcon />}
       </button>
       <button
         className="dark-mode-button"
