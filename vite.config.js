bdcdb9c705054048d668e2f889b13b2bc0412eb2
Alan Cleetus <alanclee95@gmail.com>
2024-12-14 04:02:58 -0800

    Project settings update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ["@tiptap/starter-kit"], // Specify the external packages
+    },
+  },
+});
