265e25c575998a29cb75297b2abd24d523c5cc04
Alan Cleetus <alanclee95@gmail.com>
2024-12-15 13:09:43 -0800

    adding checkboxes and trimming down text editor functionalities

--- src/components/Tiptap.jsx
+++ src/components/Tiptap.jsx
@@ -1,10 +1,22 @@
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 
-// define your extension array
-const extensions = [StarterKit];
+import TaskItem from "@tiptap/extension-task-item";
+import TaskList from "@tiptap/extension-task-list";
 
-const content = "<p>You note content...</p>";
+// define your extension array
+const extensions = [
+  StarterKit,
+  TaskList.configure({
+    HTMLAttributes: {
+      class: "my-custom-class",
+    },
+    itemTypeName: "taskItem",
+  }),
+  TaskItem,
+];
+
+const content = `<p></p>`;
 
 const Tiptap = () => {
   const editor = useEditor({ extensions, content });
--- src/components/TiptapEditor.jsx
+++ src/components/TiptapEditor.jsx
@@ -1,28 +1,19 @@
+import StarterKit from "@tiptap/starter-kit";
+import TaskItem from "@tiptap/extension-task-item";
+import TaskList from "@tiptap/extension-task-list";
+import { EditorProvider, useCurrentEditor } from "@tiptap/react";
 import ListItem from "@tiptap/extension-list-item";
 import TextStyle from "@tiptap/extension-text-style";
-import { EditorProvider, useCurrentEditor, useEditor } from "@tiptap/react";
-import StarterKit from "@tiptap/starter-kit";
-
-import BoldIcon from "remixicon-react/BoldIcon";
-import ItalicIcon from "remixicon-react/ItalicIcon";
-import StrikethroughIcon from "remixicon-react/StrikethroughIcon";
-
-import H1Icon from "remixicon-react/H1Icon";
-import H2Icon from "remixicon-react/H2Icon";
-import H3Icon from "remixicon-react/H3Icon";
-import H4Icon from "remixicon-react/H4Icon";
-import H5Icon from "remixicon-react/H5Icon";
-import H6Icon from "remixicon-react/H6Icon";
 
 import ListUnorderedIcon from "remixicon-react/ListUnorderedIcon";
 import ListOrderedIcon from "remixicon-react/ListOrderedIcon";
+import CheckboxMultipleLineIcon from "remixicon-react/CheckboxMultipleLineIcon";
 
 import SeparatorIcon from "remixicon-react/SeparatorIcon";
 
 import ArrowGoBackLineIcon from "remixicon-react/ArrowGoBackLineIcon";
 import ArrowGoForwardLineIcon from "remixicon-react/ArrowGoForwardLineIcon";
-
-function TipTapEditor({ setEditorContent, initialContent = null }) {
+const TipTapEditor = ({ setEditorContent, initialContent = null }) => {
   const MenuBar = () => {
     const { editor } = useCurrentEditor();
     if (!editor) {
@@ -30,99 +21,37 @@ function TipTapEditor({ setEditorContent, initialContent = null }) {
     }
 
     setEditorContent(editor.getHTML());
+
     return (
       <div className="control-group">
         <div className="button-group">
           <button
-            onClick={() => editor.chain().focus().toggleBold().run()}
-            disabled={!editor.can().chain().focus().toggleBold().run()}
-            className={editor.isActive("bold") ? "is-active" : ""}
-          >
-            <BoldIcon />
-          </button>
-          <button
-            onClick={() => editor.chain().focus().toggleItalic().run()}
-            disabled={!editor.can().chain().focus().toggleItalic().run()}
-            className={editor.isActive("italic") ? "is-active" : ""}
-          >
-            <ItalicIcon />
-          </button>
-          <button
-            onClick={() => editor.chain().focus().toggleStrike().run()}
-            disabled={!editor.can().chain().focus().toggleStrike().run()}
-            className={editor.isActive("strike") ? "is-active" : ""}
-          >
-            <StrikethroughIcon />
-          </button>
-          <button
-            onClick={() =>
-              editor.chain().focus().toggleHeading({ level: 1 }).run()
-            }
+            onClick={() => editor.chain().focus().toggleTaskList().run()}
             className={
-              editor.isActive("heading", { level: 1 }) ? "is-active" : ""
+              editor.isActive("taskList")
+                ? "is-active editor-button"
+                : "editor-button"
             }
           >
-            <H1Icon />
-          </button>
-          <button
-            onClick={() =>
-              editor.chain().focus().toggleHeading({ level: 2 }).run()
-            }
-            className={
-              editor.isActive("heading", { level: 2 }) ? "is-active" : ""
-            }
-          >
-            <H2Icon />
-          </button>
-          <button
-            onClick={() =>
-              editor.chain().focus().toggleHeading({ level: 3 }).run()
-            }
-            className={
-              editor.isActive("heading", { level: 3 }) ? "is-active" : ""
-            }
-          >
-            <H3Icon />
-          </button>
-          <button
-            onClick={() =>
-              editor.chain().focus().toggleHeading({ level: 4 }).run()
-            }
-            className={
-              editor.isActive("heading", { level: 4 }) ? "is-active" : ""
-            }
-          >
-            <H4Icon />
-          </button>
-          <button
-            onClick={() =>
-              editor.chain().focus().toggleHeading({ level: 5 }).run()
-            }
-            className={
-              editor.isActive("heading", { level: 5 }) ? "is-active" : ""
-            }
-          >
-            <H5Icon />
-          </button>
-          <button
-            onClick={() =>
-              editor.chain().focus().toggleHeading({ level: 6 }).run()
-            }
-            className={
-              editor.isActive("heading", { level: 6 }) ? "is-active" : ""
-            }
-          >
-            <H6Icon />
+            <CheckboxMultipleLineIcon />
           </button>
           <button
             onClick={() => editor.chain().focus().toggleBulletList().run()}
-            className={editor.isActive("bulletList") ? "is-active" : ""}
+            className={
+              editor.isActive("bulletList")
+                ? "is-active editor-button"
+                : "editor-button"
+            }
           >
             <ListUnorderedIcon />
           </button>
           <button
             onClick={() => editor.chain().focus().toggleOrderedList().run()}
-            className={editor.isActive("orderedList") ? "is-active" : ""}
+            className={
+              editor.isActive("orderedList")
+                ? "is-active editor-button"
+                : "editor-button"
+            }
           >
             <ListOrderedIcon />
           </button>
@@ -131,7 +60,6 @@ function TipTapEditor({ setEditorContent, initialContent = null }) {
           >
             <SeparatorIcon />
           </button>
-
           <button
             onClick={() => editor.chain().focus().undo().run()}
             disabled={!editor.can().chain().focus().undo().run()}
@@ -150,17 +78,15 @@ function TipTapEditor({ setEditorContent, initialContent = null }) {
   };
 
   const extensions = [
-    TextStyle.configure({ types: [ListItem.name] }),
-    StarterKit.configure({
-      bulletList: {
-        keepMarks: true,
-        keepAttributes: false,
+    StarterKit,
+    TaskList.configure({
+      HTMLAttributes: {
+        class: "my-custom-class",
       },
-      orderedList: {
-        keepMarks: true,
-        keepAttributes: false,
-      },
+      itemTypeName: "taskItem",
     }),
+    TaskItem,
+    TextStyle.configure({ types: [ListItem.name] }),
   ];
 
   const content = initialContent || `<p></p>`;
@@ -170,8 +96,8 @@ function TipTapEditor({ setEditorContent, initialContent = null }) {
       slotBefore={<MenuBar />}
       extensions={extensions}
       content={content}
-    ></EditorProvider>
+    />
   );
-}
+};
 
 export default TipTapEditor;
